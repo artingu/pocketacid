db00cb845ed3d6ab7142e35a1829b8fafefadc58
Philip Linde <philip@text.garden>
2025-10-19 00:42:30 +0200

    New colorscheme

--- src/colors.zig
+++ src/colors.zig
@@ -9,9 +9,16 @@ const Attrib = @import("CharDisplay.zig").Attrib;
 // pub const normal = Attrib{ .fg = RGB.init(0, 228, 54), .bg = bg };
 // pub const hilight = Attrib{ .fg = RGB.init(255, 236, 39), .bg = bg };
 
-pub const bg = RGB.init(48, 48, 48);
-pub const inactive = Attrib{ .fg = RGB.init(0, 0, 0), .bg = bg };
+// 80s
+// pub const bg = RGB.init(48, 48, 48);
+// pub const inactive = Attrib{ .fg = RGB.init(0, 0, 0), .bg = bg };
+// pub const playing = Attrib{ .fg = RGB.init(255, 255, 255), .bg = bg };
+// pub const time = Attrib{ .fg = RGB.init(0, 135, 81), .bg = bg };
+// pub const normal = Attrib{ .fg = RGB.init(78, 178, 212), .bg = bg };
+// pub const hilight = Attrib{ .fg = RGB.init(178, 62, 189), .bg = bg };
+
+pub const bg = RGB.init(30, 48, 28);
+pub const inactive = Attrib{ .fg = RGB.init(97, 161, 87), .bg = bg };
 pub const playing = Attrib{ .fg = RGB.init(255, 255, 255), .bg = bg };
-pub const time = Attrib{ .fg = RGB.init(0, 135, 81), .bg = bg };
-pub const normal = Attrib{ .fg = RGB.init(78, 178, 212), .bg = bg };
-pub const hilight = Attrib{ .fg = RGB.init(178, 62, 189), .bg = bg };
+pub const normal = Attrib{ .fg = RGB.init(161, 148, 87), .bg = bg };
+pub const hilight = Attrib{ .fg = RGB.init(161, 109, 87), .bg = bg };
